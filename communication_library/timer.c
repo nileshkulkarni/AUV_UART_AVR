7ff9ff76a4227bcd042783ba8b6531fd34dd4f95
Anay Joshi <anaykumar.joshi@gmail.com>
2013-03-01 13:37:11 +0530

    minor change

--- communication_library/timer.c
+++ communication_library/timer.c
@@ -5,22 +5,21 @@
 #define MOTION_CONRTROLLER_OCR (FOSC/MOTION_CONTROLLER_FREQ/1024)
 
 void timer_init(void) {
-// prescaler = 1024
+/* prescaler = 1024 */
 TCCR1B = (1 << CS02) | (1 << CS00);
-/*
-OCR1AH = 0xFF00 & MOTION_CONRTROLLER_OCR;
-OCR1AL = 0x00FF & MOTION_CONRTROLLER_OCR;
-*/
 OCR1AH = (MOTION_CONRTROLLER_OCR & 0xFF00) >> 8;
 OCR1AL = MOTION_CONRTROLLER_OCR & 0x00FF;
+/* enabling compare match interrupts */
 TIMSK1 = (1 << OCIE1A);
 TIFR1 = (1 << OCF1A);
+/* begin timer */
 TCNT1 = 0;
 }
 
 ISR (TIMER1_COMPA_vect) {
-
+/* simulating CTC mode by forcing TCNT1 = 0 */
 TCNT1 = 0;
+
 static int i = 0;
 	if (i == 0) {
 	PORTC = 0xF0;
